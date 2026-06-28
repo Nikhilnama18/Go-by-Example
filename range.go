435cd13e3ae3e3ff4f5cf8ecfa663ad14eabca5c
Nikhil_Nama <nikhilnama1111@gmail.com>
2026-06-28 11:46:54 +0530

    Range on built in types in Go

---
+++ range.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	fmt.Println("Range examples")
+
+	nums := []int{2, 4, 6, 8}
+	sum:= 0
+	for _, n := range nums {
+		sum += n
+	}
+	fmt.Println("Sum is: ", sum)
+
+	for i, n := range nums {
+		if n == 6 {
+			fmt.Println("Index of 6 is: ", i)
+		}
+	}
+
+	keyValues := map[string]string{"a": "apple", "b": "banana"}
+	for k, v := range keyValues {
+		fmt.Println(k, v)
+	}
+
+	for k:= range keyValues {
+		fmt.Println("Key is: ", k)
+	}
+
+	for i, c := range "golang" {
+		fmt.Println(i, c)
+	}
+}
